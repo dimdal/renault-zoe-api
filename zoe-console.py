b094cc18ec07f4135071c518bf537b34e3111cbd
Marius Ørvik <dimdal@gmail.com>
2018-03-17 13:58:55 +0100

    initial commit

---
+++ zoe-console.py
@@ -0,0 +1,37 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Support Python3 in Python2.
+from __future__ import print_function
+
+# All the shared functions are in this package.
+from shared.zeservices import ZEServices
+
+# This script makes heavy use of JSON parsing.
+import json
+
+# We check whether we are running on Windows or not.
+import sys
+
+# We play with encodings so it's good to check what we are set to support.
+import locale
+
+# Load credentials.
+in_file = open('credentials.json', 'r')
+credentials = json.load(in_file)
+in_file.close()
+
+# Get the VIN.
+vin = credentials['VIN']
+
+# Create a ZE Services object.
+zeServices = ZEServices(credentials['ZEServicesUsername'], credentials['ZEServicesPassword'])
+
+# ZE Services vehicle status.
+zeServices_json = zeServices.apiCall('/api/vehicle/' + vin + '/battery')
+
+battery          = zeServices_json['charge_level']
+remaining_range  = zeServices_json['remaining_range']
+chargingStatus   = zeServices_json['charging']
+pluggedStatus    = zeServices_json['plugged']
+updateTime       = zeServices_json['last_update']
