c5637eb9b4b1a0593b1b04a87829252e5b318cae
Marius Ørvik <dimdal@gmail.com>
2018-03-17 14:00:51 +0100

    strip unnecessary code

--- zoe-console.py
+++ zoe-console.py
@@ -7,11 +7,7 @@ from __future__ import print_function
 # All the shared functions are in this package.
 from shared.zeservices import ZEServices
 
-# This script makes heavy use of JSON parsing.
 import json
-
-# We check whether we are running on Windows or not.
-import sys
 
 # We play with encodings so it's good to check what we are set to support.
 import locale
@@ -29,9 +25,3 @@ zeServices = ZEServices(credentials['ZEServicesUsername'], credentials['ZEServic
 
 # ZE Services vehicle status.
 zeServices_json = zeServices.apiCall('/api/vehicle/' + vin + '/battery')
-
-battery          = zeServices_json['charge_level']
-remaining_range  = zeServices_json['remaining_range']
-chargingStatus   = zeServices_json['charging']
-pluggedStatus    = zeServices_json['plugged']
-updateTime       = zeServices_json['last_update']
